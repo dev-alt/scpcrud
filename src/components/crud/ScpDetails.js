1bab218b6f2a975804cb947e555511805855ba4d
Andrew <andrew.logan@hotmail.com>
2023-10-12 15:46:30 +1300

    v1

--- src/components/crud/ScpDetails.js
+++ src/components/crud/ScpDetails.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-import { Box, Typography, Paper, IconButton,Tooltip, Zoom } from '@mui/material';
+import { Box, Typography, Paper, IconButton, Tooltip, Zoom } from '@mui/material';
 import { db } from '../../utils/DbConfig';
 import { collection, doc, getDoc } from 'firebase/firestore';
 import { useParams } from 'react-router-dom';
@@ -29,46 +29,73 @@ function Detail() {
         <Box>
             <Typography variant='h6' sx={{ textAlign: 'center', color: 'primary' }}>SCP Entry Details</Typography>
             <Zoom in timeout={2000}>
-            <Paper elevation={3} sx={{ p: 3, m: 3 }}>
-                <Box sx={{ mt: 0, textAlign: 'center', color: 'error.main' }}>
+                <Paper elevation={3} sx={{ p: 3, m: 3 }}>
+                    <Box sx={{ mt: 0, textAlign: 'center', color: 'error.main' }}>
 
-                </Box>
-                
-                {scpDetails ? (
-                    <Box>
-                        <Typography variant="h5" sx={{ mb: 1 }}>{scpDetails.Number}</Typography>
-                        <Typography variant="h2" sx={{ mb: 2 }}>{scpDetails.Name}</Typography>
-                        <Typography variant="caption" sx={{ mb: 2 }}>{scpDetails.ObjectClass}</Typography>
-                        <Typography variant='h5' sx={{ mb: 3 }}>Special Containment Procedures: </Typography>
-                        <Typography>{scpDetails.Containment}</Typography>
-                        <Typography variant='h5' sx={{ mt: 2,mb:1 }}>Description: </Typography>
-                        <Typography>{scpDetails.Description}</Typography>
-                        <Tooltip title="Edit">
-                        <IconButton
-                            variant="text"
-                            color="primary"
-                            component={Link}
-                            to={`/update/${scpDetails.id}`}
-                        >
-                            <EditNoteIcon />
-                        </IconButton>
-                        </Tooltip>
-                        <Tooltip title="Delete">
-                        <IconButton
-                            variant="text"
-                            color='error'
-                            
-                            component={Link}
-                            to={`/delete/${scpDetails.id}`}
-                        >        <DeleteIcon />
-                          
-                        </IconButton>
-                        </Tooltip>
                     </Box>
-                ) : (
-                    <p>SCP Entry not found.</p>
-                )}
-            </Paper></Zoom>
+
+                    {scpDetails ? (
+                        <Box>
+                            <Typography variant="h3" sx={{ mb: 1 }}>{scpDetails.Number}</Typography>
+                            <Typography variant="h6" sx={{ mb: 2 }}>{scpDetails.Name}</Typography>
+                            <Typography variant="caption" sx={{ mb: 2 }}>{scpDetails.ObjectClass}</Typography>
+                            <Typography variant='h5' sx={{ mb: 3 }}>Special Containment Procedures: </Typography>
+                            <Typography>{scpDetails.Containment}</Typography>
+                            <Typography variant='h5' sx={{ mt: 2, mb: 1 }}>Description: </Typography>
+                            <Typography>{scpDetails.Description}</Typography>
+                            {scpDetails.AddendumText && (
+            <div>
+                <Typography variant='h5' sx={{ mt: 2, mb: 1 }}>Addendum Text:</Typography>
+                <Typography>{scpDetails.AddendumText}</Typography>
+            </div>
+        )}
+
+        {scpDetails.HistoryText && (
+            <div>
+                <Typography variant='h5' sx={{ mt: 2, mb: 1 }}>History Text:</Typography>
+                <Typography>{scpDetails.HistoryText}</Typography>
+            </div>
+        )}
+
+        {scpDetails.NotesText && (
+            <div>
+                <Typography variant='h5' sx={{ mt: 2, mb: 1 }}>Notes Text:</Typography>
+                <Typography>{scpDetails.NotesText}</Typography>
+            </div>
+        )}
+
+        {scpDetails.ReferencesText && (
+            <div>
+                <Typography variant='h5' sx={{ mt: 2, mb: 1 }}>References Text:</Typography>
+                <Typography>{scpDetails.ReferencesText}</Typography>
+            </div>
+        )}
+                            <Tooltip title="Edit">
+                                <IconButton
+                                    variant="text"
+                                    color="primary"
+                                    component={Link}
+                                    to={`/update/${scpDetails.id}`}
+                                >
+                                    <EditNoteIcon />
+                                </IconButton>
+                            </Tooltip>
+                            <Tooltip title="Delete">
+                                <IconButton
+                                    variant="text"
+                                    color='error'
+
+                                    component={Link}
+                                    to={`/delete/${scpDetails.id}`}
+                                >        <DeleteIcon />
+
+                                </IconButton>
+                            </Tooltip>
+                        </Box>
+                    ) : (
+                        <p>SCP Entry not found.</p>
+                    )}
+                </Paper></Zoom>
         </Box>
     );
 }
